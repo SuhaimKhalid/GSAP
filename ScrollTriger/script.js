19729c8a2c0a4b138b61263c674e8a97a3040b7a
Suhaim Khalid <suhaimkhalid007@gmail.com>
2026-03-01 23:10:46 +0000

    Create scroll feature

--- ScrollTriger/script.js
+++ ScrollTriger/script.js
@@ -13,3 +13,48 @@ tl.from(".innerNav > ul> li", {
   duration: 1.5,
   stagger: 0.3,
 });
+
+gsap.from("#page1 #box1", {
+  scale: 0,
+  duration: 2,
+});
+
+gsap.from("#page2 h3", {
+  y: -30,
+  opacity: 0,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: "#ph3",
+    scroller: "body",
+
+    start: "top 60%",
+    end: "top 20%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#page2 #box2", {
+  x: -900,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: "#box2",
+    scroller: "body",
+
+    start: "top 80%",
+    end: "top 40%",
+    scrub: 5,
+  },
+});
+
+gsap.to("#page3 h1", {
+  transform: "translateX(-59%)",
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "body",
+    markers: true,
+    scrub: 5,
+    start: "top 0%",
+    end: "top -100%",
+    pin: true,
+  },
+});
